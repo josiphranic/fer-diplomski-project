68c03962eb0faf2fb402432ac16640b351058035
Josip Hranic <josip.hranic96@gmail.com>
2020-05-11 08:44:46 +0200

    custom generalized dice loss function, params update

--- project/custom_2D_unet_helpers.py
+++ project/custom_2D_unet_helpers.py
@@ -22,8 +22,18 @@ def jaccard_loss(y_true, y_pred, smooth=100):
     return (1 - jac) * smooth
 
 
-def dice_coef(y_true, y_pred, smooth=1):
+def dice_coef(y_true, y_pred, smooth=1.):
     y_true_f = K.flatten(y_true)
     y_pred_f = K.flatten(y_pred)
     intersection = K.sum(y_true_f * y_pred_f)
     return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+
+def custom_generalized_dice_loss(y_true, y_pred):
+    number_of_classes = K.shape(y_true).shape[-1]
+    weights = [1. / K.sum(y_true[:, :, :, class_index]) for class_index in range(number_of_classes)]
+    weights = [weight / K.sum(weights) for weight in weights]
+    dice_coefs = [weights[class_index] * dice_coef(y_true[:, :, :, class_index], y_pred[:, :, :, class_index]) for class_index in range(number_of_classes)]
+    outlines = [K.mean((K.ones(K.shape(y_true[:, :, :, class_index])) - y_true[:, :, :, class_index]) * y_pred[:, :, :, class_index]) for class_index in range(number_of_classes)]
+    outlines = [weights[class_index] * outlines[class_index] for class_index in range(number_of_classes)]
+    return 1. - K.sum(dice_coefs) + K.sum(outlines)
--- project/main.py
+++ project/main.py
@@ -5,17 +5,19 @@ from tensorflow.keras.callbacks import ModelCheckpoint, TensorBoard, EarlyStoppi
 from tensorflow.keras.utils import plot_model
 
 dataset_root_dir = '/workspace/datasets/kbc_sm_splited/'
-results_root_dir = '/workspace/results/kbc_sm/'
-pretrained_model_path = '/workspace/results/sfu/results/2020-04-21_02:34:32.802993/unet_jaccard.hdf5'
+results_root_dir = '/workspace/results/kbc_sm_pretrained_encoder_vgg19/'
+# pretrained_model_path = '/workspace/results/sfu/results/2020-04-21_02:34:32.802993/unet_jaccard.hdf5'
 input_shape = (512, 256, 3)
 mask_pixel_values_aka_classes = [0, 45, 125, 205]
 # mask_pixel_values_aka_classes = [0, 64, 80, 100, 120, 192, 255]
 number_of_classes = len(mask_pixel_values_aka_classes)
-batch_size = 3
+batch_size = 10
 epochs = 1000
-early_stopping_patience = 15
+early_stopping_patience = 50
+loss_function = custom_generalized_dice_loss
 description = 'epochs:' + str(epochs) + ' input shape:' + str(input_shape) + ' number of classes:' + str(number_of_classes) + ' batch size:' + str(batch_size)
 description += ' early stopping patience:' + str(early_stopping_patience)  # + ' pretrained model:' + pretrained_model_path
+description += ' loss function:' + loss_function.__name__
 
 train_images, train_masks = load_and_preprocess_train_images_and_masks(dataset_root_dir + 'train', 'image', 'label', mask_pixel_values_aka_classes, shape=input_shape)
 test_images, test_masks = load_and_preprocess_test_images_and_masks(dataset_root_dir + 'test', 'image', 'label', mask_pixel_values_aka_classes, shape=input_shape)
@@ -23,11 +25,11 @@ test_images, test_masks = load_and_preprocess_test_images_and_masks(dataset_root
 # model = custom_unet(input_shape, num_classes=number_of_classes, use_batch_norm=True, output_activation='softmax')
 # model = get_custom_model_with_frozen_encoder(pretrained_model_path, number_of_classes)
 model = custom_unet_with_vgg19_encoder(input_shape, num_classes=number_of_classes, use_batch_norm=True, output_activation='softmax')
-model.compile(optimizer='adam', loss=jaccard_loss, metrics=['accuracy', dice_coef])
+model.compile(optimizer='adam', loss=loss_function, metrics=['accuracy', dice_coef])
 model.summary()
 
 results_dir = create_results_dir_results_predict_dir_and_logs_dir(results_root_dir)
-model_checkpoint = ModelCheckpoint(results_dir + 'unet_jaccard.hdf5', monitor='loss', verbose=1, save_best_only=True)
+model_checkpoint = ModelCheckpoint(results_dir + 'unet.hdf5', monitor='loss', verbose=1, save_best_only=True)
 tensorboard = TensorBoard(results_dir + 'tensorboardlogs/', histogram_freq=1)
 early_stopping = EarlyStopping(monitor='val_loss', verbose=1, patience=early_stopping_patience)
 model.fit(train_images, train_masks, batch_size=batch_size, epochs=epochs, callbacks=[model_checkpoint, tensorboard, early_stopping], validation_split=0.15, shuffle=True)
